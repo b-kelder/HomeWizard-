888a74c6083d36edfa70bbef6b1035dc8d95c8b2
bram kelder <bram-kelder@hotmail.com>
2016-05-18 12:00:10 +0200

    Updates to homewizzzzz switches

--- HomeWizard++/MQTT test app/app/src/main/java/idu/stenden/inf1i/homewizard/MainActivity.java
+++ HomeWizard++/MQTT test app/app/src/main/java/idu/stenden/inf1i/homewizard/MainActivity.java
@@ -1,7 +1,13 @@
 package idu.stenden.inf1i.homewizard;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Looper;
+import android.os.Message;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,8 +17,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,21 +36,36 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    MqttAndroidClient client;
-    boolean connectSucces = false;
+    private MqttAndroidClient client;
+    private boolean connectSucces = false;
+    private ListView mainListView;
+    private ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        client =  new MqttAndroidClient(getApplicationContext(), "tcp://10.110.111.141", "HomeWizard++");
-        //client =  new MqttAndroidClient(getApplicationContext(), "tcp://test.mosquitto.org", "HomeWizard++");
+
+        //client =  new MqttAndroidClient(getApplicationContext(), "tcp://10.110.111.141", "HomeWizard++");
+        client =  new MqttAndroidClient(getApplicationContext(), "tcp://test.mosquitto.org", "HomeWizard++");
 
         try {
             client.connect(null, new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
                     connectSucces = true;
+                    try {
+                        client.subscribe("HMWZRETURN/#", 0);
+                        client.subscribe("HMWZRETURN", 0);
+                        mqtt("HMWZ", "get-sensors");
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    }
                 }
 
                 @Override
@@ -53,30 +77,39 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             e.printStackTrace();
         }
 
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Switch knopje = (Switch) findViewById(R.id.knopje);
+        mainListView = (ListView) findViewById( R.id.mainListView );
 
-        assert knopje != null;
-        knopje.setOnCheckedChangeListener((new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // The toggle is enabled
-                    mqtt("HMWZ/sw/1", "on");
+        // Create and populate a List of planet names.
+        ArrayList<String> itemsList = new ArrayList<String>();
 
-                } else {
-                    // The toggle is disabled
-                    mqtt("HMWZ/sw/1", "off");
-                }
-            }
-        }));
+        listAdapter = new ArrayAdapter<String>(this, R.layout.row, R.id.rowTextView , itemsList);
+
+        mainListView.setAdapter( listAdapter );
+
+
+
+        //Switch knopje = (Switch) findViewById(R.id.knopje);
+
+        //assert knopje != null;
+        //knopje.setOnCheckedChangeListener((new CompoundButton.OnCheckedChangeListener() {
+        //    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        //        if (isChecked) {
+        //            The toggle is enabled
+        //            mqtt("HMWZ/sw/1", "on");
+        //
+        //        } else {
+        //            The toggle is disabled
+        //            mqtt("HMWZ/sw/1", "off");
+        //        }
+        //    }
+        //}));
 
         Button button = (Button) findViewById(R.id.testbutton);
-        assert button != null;
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 mqtt("HMWZ", "get-sensors");
@@ -163,21 +196,26 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             @Override
             public void messageArrived(String topic, MqttMessage message) throws Exception {
 
-                TextView text = (TextView) findViewById(R.id.textView3);
-                text.setText(message.toString());
-                TextView text2 = (TextView) findViewById(R.id.textView4);
-
                 JSONObject json = new JSONObject(message.toString());
-                json = json.getJSONObject("response");
+                json = json.getJSONObject("request");
+                String route = json.getString("route");
 
-                JSONArray array = json.getJSONArray("switches");
+                if (route.equals("/get-sensors")){
+                    listAdapter.clear();
 
-                for (int i = 0; i < 3; i++){
-                    JSONObject Swagtestsysteem = array.getJSONObject(i);
+                    json = new JSONObject(message.toString());
+                    json = json.getJSONObject("response");
+                    JSONArray array = json.getJSONArray("switches");
 
-                    String name = Swagtestsysteem.getString("name");
-                    String status = Swagtestsysteem.getString("status");
-                    text2.append(name + ": " + status + " ");
+                    for (int i = 0; i < array.length(); i++){
+                        JSONObject Swagtestsysteem = array.getJSONObject(i);
+
+                        String name = Swagtestsysteem.getString("name");
+                        String status = Swagtestsysteem.getString("status");
+                        listAdapter.add(name);
+                    }
+                } else {
+                    Log.d("Route", route);
                 }
             }
 
@@ -193,8 +231,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         if(connectSucces){
             try {
-                client.subscribe("HMWZRETURN", 0);
-                client.subscribe("HMWZRETURN/#", 0);
                 client.publish(topic, message);
                 Toast toast = Toast.makeText(getApplicationContext(), "MQTT message send", Toast.LENGTH_SHORT);
                 toast.show();
