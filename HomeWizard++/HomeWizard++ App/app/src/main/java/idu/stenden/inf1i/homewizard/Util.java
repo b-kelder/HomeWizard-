9eee33d172154afcc39d8490167c2c89fb48eddc
AukeM <aukemuller@hotmail.com>
2016-05-30 13:12:45 +0200

    App: custommqtt Array Import

--- HomeWizard++/HomeWizard++ App/app/src/main/java/idu/stenden/inf1i/homewizard/Util.java
+++ HomeWizard++/HomeWizard++ App/app/src/main/java/idu/stenden/inf1i/homewizard/Util.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
