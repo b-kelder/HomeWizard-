922b1127c1c05227b1141dbad3bb84de5dc8763c
AukeM <aukemuller@hotmail.com>
2016-05-30 13:21:18 +0200

    App: customMqtt missing import

--- HomeWizard++/HomeWizard++ App/app/src/main/java/idu/stenden/inf1i/homewizard/AppDataContainer.java
+++ HomeWizard++/HomeWizard++ App/app/src/main/java/idu/stenden/inf1i/homewizard/AppDataContainer.java
@@ -6,6 +6,7 @@ import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
