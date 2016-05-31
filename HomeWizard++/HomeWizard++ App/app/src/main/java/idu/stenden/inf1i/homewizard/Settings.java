41a9a5f741738d8253ef65ba55aee69b83f4785c
bram kelder <bram-kelder@hotmail.com>
2016-05-31 14:26:39 +0200

    Revert "Revert "werkt nu als het goed is""
    This reverts commit 1f187dfc851d3a6e9e24a790acc8f43cd97d46f4.

--- HomeWizard++/HomeWizard++ App/app/src/main/java/idu/stenden/inf1i/homewizard/Settings.java
+++ HomeWizard++/HomeWizard++ App/app/src/main/java/idu/stenden/inf1i/homewizard/Settings.java
@@ -109,9 +109,17 @@ public class Settings extends BaseMqttEventActivity{
                         Util.saveAdminPin(Settings.context, adminPinTxt.getText().toString(), true);
                         Toast.makeText(Settings.this, "Pin code saved", Toast.LENGTH_LONG).show();
                     }
-                    else if(adminPinTxt.getText().toString().isEmpty() && adminPinButton.isChecked())
+                    else
                     {
-                        Toast.makeText(Settings.this, "Please enter a code", Toast.LENGTH_LONG).show();
+                        if(!adminPin.isEmpty() && !adminPinButton.isChecked())
+                        {
+                            Util.saveAdminPin(Settings.context, "", false);
+                            Toast.makeText(Settings.this, "Pin code removed", Toast.LENGTH_LONG).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(Settings.this, "Please enter a code", Toast.LENGTH_LONG).show();
+                        }
                     }
                 }
                 catch(Exception e)
@@ -129,13 +137,6 @@ public class Settings extends BaseMqttEventActivity{
                     applyPin.setEnabled(true); // enable apply pin button
                 } else {
                     adminPinTxt.setEnabled(false); // disable pin text input
-                    applyPin.setEnabled(false); // disable apply pin button
-
-                    if(!adminPin.isEmpty())
-                    {
-                        Util.saveAdminPin(Settings.context, "", false);
-                        Toast.makeText(Settings.this, "Pin code disabled", Toast.LENGTH_LONG).show();
-                    }
                 }
             }
         }));
@@ -168,7 +169,6 @@ public class Settings extends BaseMqttEventActivity{
 
         try {
             JSONObject loginSettingsFile = Util.readLoginData(this);
-            serial = loginSettingsFile.getString("serial");
             emailField.setText(loginSettingsFile.getString("email"));
             passwordField.setText(loginSettingsFile.getString("password"));
 
