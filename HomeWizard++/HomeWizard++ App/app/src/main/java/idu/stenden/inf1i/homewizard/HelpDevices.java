da0ad1895c65b883230e5262483b73e5e755ddc1
AukeM <aukemuller@hotmail.com>
2016-06-17 11:23:57 +0200

    App: rework & improvement help page

---
+++ HomeWizard++/HomeWizard++ App/app/src/main/java/idu/stenden/inf1i/homewizard/HelpDevices.java
@@ -0,0 +1,15 @@
+package idu.stenden.inf1i.homewizard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelpDevices extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_devices);
+    }
+}
--- HomeWizard++/HomeWizard++ App/app/src/main/java/idu/stenden/inf1i/homewizard/HelpPage.java
+++ HomeWizard++/HomeWizard++ App/app/src/main/java/idu/stenden/inf1i/homewizard/HelpPage.java
@@ -1,7 +1,10 @@
 package idu.stenden.inf1i.homewizard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HelpPage extends AppCompatActivity
 {
@@ -10,5 +13,33 @@ public class HelpPage extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_page);
+
+        Button btnConfig = (Button) findViewById(R.id.btnConfig);
+        Button btnAddRemove = (Button) findViewById(R.id.btnDeviceAddRemove);
+        Button btnReturn = (Button) findViewById(R.id.btnDeviceReturn);
+
+        btnConfig.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                startActivity(new Intent(getApplicationContext(), HelpConfig.class));
+            }
+        });
+
+        btnAddRemove.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                startActivity(new Intent(getApplicationContext(), HelpDevices.class));
+            }
+        });
+
+        btnReturn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                startActivity(new Intent(getApplicationContext(), HelpReturn.class));
+            }
+        });
     }
 }
